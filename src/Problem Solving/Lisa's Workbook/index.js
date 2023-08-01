86ab6952265c5e65e32aada48c0a5dd854064b64
AndrewMamdouh <andrew.elking99@gmail.com>
2023-08-01 22:28:50 +0300

    Add "Counting Sort 2" + Formatting

--- src/Problem Solving/Lisa's Workbook/index.js
+++ src/Problem Solving/Lisa's Workbook/index.js
@@ -1,26 +1,31 @@
-/** 
+/**
  * Counts the number of special problems in the workbook
  * @author Andrew Mamdouh <andrewm.developer@gmail.com>
  * @param {number} n - The number of chapters
  * @param {number} k - The maximum number of problems per page
  * @param {number[]} arr - The number of problems in each chapter
  * @returns {number} The number of special problems in the workbook
-*/
+ */
 function workbook(n, k, arr) {
-    let pageNum = 0;
-    
-    const detailedArr = arr.reduce((acc, cur) => {
-        const problemsArr = Array(Math.floor(cur/k)).fill(k);
-        acc.push(cur % k ? [...problemsArr, cur % k] : problemsArr);
-        return acc;
-    }, []);
-    
-    return detailedArr.reduce((acc, curArr) => acc + curArr.reduce((arrAcc, curArrElem, curArrIdx) => {
+  let pageNum = 0;
+
+  const detailedArr = arr.reduce((acc, cur) => {
+    const problemsArr = Array(Math.floor(cur / k)).fill(k);
+    acc.push(cur % k ? [...problemsArr, cur % k] : problemsArr);
+    return acc;
+  }, []);
+
+  return detailedArr.reduce(
+    (acc, curArr) =>
+      acc +
+      curArr.reduce((arrAcc, curArrElem, curArrIdx) => {
         pageNum++;
         const lowerBound = curArrIdx * k + 1;
         const upperBound = lowerBound + curArrElem - 1;
         return arrAcc + +(lowerBound <= pageNum && upperBound >= pageNum);
-    }, 0), 0);
+      }, 0),
+    0
+  );
 }
 
 module.exports = workbook;
--- src/Problem Solving/Lisa's Workbook/index.test.js
+++ src/Problem Solving/Lisa's Workbook/index.test.js
@@ -1,5 +1,5 @@
-const workbook = require('.');
+const workbook = require(".");
 
-test('should return the number of special problems in the workbook', () => {
-    expect(workbook(5, 3, [4, 2, 6, 1, 10])).toBe(4);
+test("should return the number of special problems in the workbook", () => {
+  expect(workbook(5, 3, [4, 2, 6, 1, 10])).toBe(4);
 });
