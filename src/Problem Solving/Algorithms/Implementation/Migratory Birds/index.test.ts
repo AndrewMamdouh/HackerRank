e72a36015835ab4dbd78515c5ffe80621042c370
AndrewMamdouh <68063809+AndrewMamdouh@users.noreply.github.com>
2024-01-12 06:07:49 +0200

    Add "Migratory Birds"

---
+++ src/Problem Solving/Algorithms/Implementation/Migratory Birds/index.test.ts
@@ -0,0 +1,5 @@
+import { migratoryBirds } from '@ProblemSolving/Algorithms/Implementation'
+
+test('should return the id of the most frequently sighted bird type', () => {
+    expect(migratoryBirds([1, 4, 4, 4, 5, 3])).toBe(4)
+})
