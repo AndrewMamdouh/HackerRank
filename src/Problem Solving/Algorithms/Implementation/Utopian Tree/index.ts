dce280c2098db6b77846f0a762cd91aa4d2761a0
AndrewMamdouh <68063809+AndrewMamdouh@users.noreply.github.com>
2024-01-13 07:38:03 +0200

    Add "Utopian Tree"

---
+++ src/Problem Solving/Algorithms/Implementation/Utopian Tree/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Finds out how tall will the tree be after certain growth cycles
+ * @author Andrew Mamdouh <andrewm.developer@gmail.com>
+ * @param {number} n - The number of growth cycles to simulate
+ * @returns {number} The height of the tree after the given number of cycles
+ */
+const utopianTree = (n: number): number => {
+    if (!n) return 1
+    if (n % 2) return utopianTree(n - 1) * 2
+    return utopianTree(n - 1) + 1
+}
+
+export default utopianTree
