4c31cccb77680b3213faf36d3ae4df2a01602b66
AndrewMamdouh <68063809+AndrewMamdouh@users.noreply.github.com>
2024-01-13 07:02:09 +0200

    Add "Counting Valleys"

---
+++ src/Problem Solving/Algorithms/Implementation/Counting Valleys/index.test.ts
@@ -0,0 +1,5 @@
+import { countingValleys } from '@ProblemSolving/Algorithms/Implementation'
+
+test('should return the number of valleys traversed', () => {
+    expect(countingValleys(8, 'UDDDUDUU')).toBe(1)
+})
