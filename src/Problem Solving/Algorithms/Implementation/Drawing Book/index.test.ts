5cf3665ef70b4618f90473885f8da2ff3d8f85b9
AndrewMamdouh <68063809+AndrewMamdouh@users.noreply.github.com>
2024-01-13 06:39:48 +0200

    Add "Drawing Book"

---
+++ src/Problem Solving/Algorithms/Implementation/Drawing Book/index.test.ts
@@ -0,0 +1,5 @@
+import { pageCount } from '@ProblemSolving/Algorithms/Implementation'
+
+test('should return the number of pages to turn', () => {
+    expect(pageCount(6, 2)).toBe(1)
+})
---
+++ src/Problem Solving/Algorithms/Implementation/Drawing Book/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Finds the minimum number of pages that must be turned in order to arrive at page p.
+ * @author Andrew Mamdouh <andrewm.developer@gmail.com>
+ * @param {number[]} n - The number of pages in the book
+ * @param {number[]} p - The page number to turn to
+ * @returns {number} The minimum number of pages to turn
+ */
+const pageCount = (n: number, p: number): number =>
+    Math.min(Math.floor(p / 2), Math.floor((n - p + +(n % 2 === 0)) / 2))
+
+export default pageCount
