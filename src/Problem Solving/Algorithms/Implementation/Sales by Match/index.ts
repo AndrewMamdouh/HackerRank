2d692ffa66a636b6cad1588ee98b48e519af548e
AndrewMamdouh <68063809+AndrewMamdouh@users.noreply.github.com>
2024-01-13 06:12:32 +0200

    Add "Sales by Match"

---
+++ src/Problem Solving/Algorithms/Implementation/Sales by Match/index.ts
@@ -0,0 +1,17 @@
+import { countDuplicates } from '@Helpers'
+/**
+ * Determines how many pairs of socks with matching colors there are
+ * @author Andrew Mamdouh <andrewm.developer@gmail.com>
+ * @param {number[]} n - The number of socks in the pile
+ * @param {number} ar - The colors of each sock
+ * @returns {number} The number of pairs
+ */
+const sockMerchant = (n: number, ar: number[]): number => {
+    const colorPairs = countDuplicates(ar)
+    return Object.values(colorPairs).reduce(
+        (acc, cur) => acc + Math.floor(cur / 2),
+        0
+    )
+}
+
+export default sockMerchant
