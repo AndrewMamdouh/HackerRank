d6ea84f7e1bb0a2a0e0837c39b0bb44f39ae5286
AndrewMamdouh <68063809+AndrewMamdouh@users.noreply.github.com>
2024-01-18 01:06:43 +0200

    Add "Find Digits"

---
+++ src/Problem Solving/Algorithms/Implementation/Find Digits/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Counts the number of divisors occurring within the integer
+ * @author Andrew Mamdouh <andrewm.developer@gmail.com>
+ * @param {number} n - The value to analyze
+ * @returns {number} The number of digits in (n) that are divisors of (n)
+ */
+const findDigits = (n: number): number => {
+    const numDigits = n
+        .toString()
+        .split('')
+        .map((digit) => +digit)
+    let count = 0
+    for (const digit of numDigits) if (n % digit === 0) count++
+    return count
+}
+
+export default findDigits
