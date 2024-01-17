7481d9fec792964dab0e80cc08b29b718ccbdd4a
AndrewMamdouh <68063809+AndrewMamdouh@users.noreply.github.com>
2024-01-17 23:00:51 +0200

    Add "Viral Advertising"

---
+++ src/Problem Solving/Algorithms/Implementation/Viral Advertising/index.ts
@@ -0,0 +1,19 @@
+/**
+ * Determines how many people have liked the ad by the end of a given day, beginning with launch day as day 1
+ * @author Andrew Mamdouh <andrewm.developer@gmail.com>
+ * @param {number} n - The day number to report
+ * @returns {number} The cumulative likes at that day
+ */
+const viralAdvertising = (n: number): number => {
+    let curLikes: number,
+        totalLikes = 0,
+        exposes = 5
+    for (let i = 0; i < n; i++) {
+        curLikes = Math.floor(exposes / 2)
+        totalLikes += curLikes
+        exposes = curLikes * 3
+    }
+    return totalLikes
+}
+
+export default viralAdvertising
