2838bd26f01af70339018151027cba82e03c5916
AndrewMamdouh <68063809+AndrewMamdouh@users.noreply.github.com>
2024-04-10 03:10:28 +0200

    Add "Fair Rations"

---
+++ src/Problem Solving/Algorithms/Implementation/Fair Rations/index.test.ts
@@ -0,0 +1,5 @@
+import { fairRations } from '@ProblemSolving/Algorithms/Implementation'
+
+test('should return the minimum number of loaves required', () => {
+    expect(fairRations([2, 3, 4, 5, 6])).toBe(4)
+})
