9b6b79f129cb31ba9fd1c7b8d38328fd73c74cee
AndrewMamdouh <68063809+AndrewMamdouh@users.noreply.github.com>
2024-09-07 00:57:26 +0300

    test(hackerrank): add test cases for "Extra Long Factorials"

---
+++ src/Problem Solving/Algorithms/Implementation/Extra Long Factorials/index.test.ts
@@ -0,0 +1,7 @@
+import { extraLongFactorials } from '@ProblemSolving/Algorithms/Implementation';
+
+describe('Problem Solving » Algorithms » Implementation » Extra Long Factorials', () => {
+    it('should return the factorial', () => {
+        expect(extraLongFactorials(25)).toBe('15511210043330985984000000');
+    });
+});
